05ec07b922ebf20432077e264faaf353062c9ffd
Jaakko Husso <jaakko.husso@futurice.com>
2017-12-29 23:50:44 +0200

    Disable origin check for now

--- config/prod.exs
+++ config/prod.exs
@@ -16,7 +16,7 @@ use Mix.Config
 config :partypeli, PartypeliWeb.Endpoint,
   load_from_system_env: true,
   url: [scheme: "https", host: System.get_env("HOST"), port: 443],
-  check_origin: ["//#{System.get_env("HOST")}"],
+  check_origin: false,
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
